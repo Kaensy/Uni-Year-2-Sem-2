80aa796e571070f6004bf72daedf02633a35b337
Kaensy <LaurentiuC_S@yahoo.com>
2024-03-11 18:14:41 +0200

    WebDev + MPP homework

---
+++ Medii de Proiectare si Programare/HW/MPP_HW_CSharp/MPP_HW_CSharp/Domain/Track.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MPP_HW_CSharp.Domain
+{
+    public class Track : Entity<String>
+    {
+        private string Name { get; set; }
+        private int MinimumAge { get; set; }
+        private int MaximumAge { get; set; }
+        private int Distance { get; set; }
+        private List<Child> Children { get; set; }
+        
+        public Track(string id, string name, int minimumAge, int maximumAge, int distance) : base(id)
+        {
+            Name = name;
+            this.MinimumAge = minimumAge;
+            this.MaximumAge = maximumAge;
+            this.Distance = distance;
+            Children = new List<Child>();
+        }
+        
+        
+    }
+}
---
+++ Medii de Proiectare si Programare/HW/mpp_hw/src/main/java/mpp/com/Domain/Child.java
@@ -0,0 +1,38 @@
+package mpp.com.Domain;
+
+import java.util.List;
+
+public class Child extends Entity<Long>{
+    private String name;
+    private int age;
+    private List<Track> tracks;
+
+    public Child(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Track> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(List<Track> tracks) {
+        this.tracks = tracks;
+    }
+}
