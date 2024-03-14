402245439519f7f100cad03473b1d273cd500909
Kaensy <LaurentiuC_S@yahoo.com>
2024-03-14 02:56:15 +0200

    AI - Lab2

---
+++ Inteligenta artificiala/Lab2/main.py
@@ -0,0 +1,143 @@
+from datetime import datetime, timedelta
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def number_of_employees():
+    df_employees = pd.read_csv('employees.csv')
+    print('Number of employees : ', df_employees.shape[0])
+
+
+def info():
+    df_employees = pd.read_csv('employees.csv')
+    print(df_employees.shape[1], df_employees.columns.to_list())
+
+
+def completed_rows():
+    df_employees = pd.read_csv('employees.csv')
+    df_no_empty_fields = df_employees.dropna(how='any')
+    num_rows_no_empty_fields = df_no_empty_fields.shape[0]
+    print("Number of rows without any empty fields: ", num_rows_no_empty_fields)
+
+
+def min_max_mean_values():
+    df_employees = pd.read_csv('employees.csv')
+    numeric_cols = df_employees.select_dtypes(include=[np.number]).columns.to_list()
+
+    min_values = df_employees[numeric_cols].min()
+    max_values = df_employees[numeric_cols].max()
+    mean_values = df_employees[numeric_cols].mean()
+
+    df_employees['Start Date'] = pd.to_datetime(df_employees['Start Date'])
+
+    df_employees['Last Login Time'] = df_employees['Last Login Time'].apply(
+        lambda x: (datetime.strptime(x, '%I:%M %p') - datetime.strptime('12:00 AM', '%I:%M %p')).seconds)
+
+    columns = ['Start Date', 'Last Login Time']
+
+    min_values_date = df_employees[columns].min()
+    max_values_date = df_employees[columns].max()
+    mean_values_date = df_employees[columns].mean()
+
+    min_values_date['Start Date'] = min_values_date['Start Date'].date()
+    max_values_date['Start Date'] = max_values_date['Start Date'].date()
+    mean_values_date['Start Date'] = mean_values_date['Start Date'].date()
+
+    min_values_date['Last Login Time'] = str(timedelta(seconds=int(min_values_date['Last Login Time'])))
+    max_values_date['Last Login Time'] = str(timedelta(seconds=int(max_values_date['Last Login Time'])))
+    mean_values_date['Last Login Time'] = str(timedelta(seconds=int(mean_values_date['Last Login Time'])))
+
+    min_values = pd.concat([min_values, min_values_date])
+    max_values = pd.concat([max_values, max_values_date])
+    mean_values = pd.concat([mean_values, mean_values_date])
+
+    result = pd.concat([min_values, max_values, mean_values], axis=1)
+    result.columns = ['Minimum', 'Maximum', 'Median']
+    print(result)
+
+
+def unique_values():
+    df_employees = pd.read_csv('employees.csv')
+    non_numerical_columns = df_employees.select_dtypes(exclude=[np.number])
+
+    unique_values = non_numerical_columns.nunique()
+
+    print(unique_values)
+
+
+def valori_lipsa():
+    df_employees = pd.read_csv('employees.csv')
+    print(df_employees.isnull().sum().to_string() + '\n')
+    print(df_employees.fillna("N/A"))
+
+
+# distributia salariilor acestor angajati pe categorii de salar
+def distributia_salariilor():
+    df_employees = pd.read_csv('employees.csv')
+
+    salary_bins = df_employees['Salary'].value_counts(bins=10, sort=False)
+
+    plt.figure(figsize=(10, 6))
+    plt.hist(df_employees['Salary'], bins=10, edgecolor='black')
+    plt.title('Salary Distribution')
+    plt.xlabel('Salary')
+    plt.ylabel('Number of Employees')
+    plt.show()
+
+
+def distributia_salariilor_echipe():
+    df_employees = pd.read_csv('employees.csv')
+
+    bins = [25000, 50000, 75000, 100000, 125000, np.inf]
+    labels = ['25k-50k', '50k-75k', '75k-100k', '100k-125k', '125k+']
+
+    df_employees['Salary Group'] = pd.cut(df_employees['Salary'], bins=bins, labels=labels)
+
+    plt.figure(figsize=(10, 6))
+    sns.countplot(data=df_employees, x='Team', hue='Salary Group')
+    plt.title('Salary Distribution by Team')
+    plt.xlabel('Team')
+    plt.ylabel('Number of Employees')
+    plt.xticks(rotation=90)
+    plt.show()
+
+
+def outliners():
+    df_employees = pd.read_csv('employees.csv')
+
+    df_employees = df_employees.sort_values('Salary')
+
+    bottom_5 = df_employees.head(5)
+    top_5 = df_employees.tail(5)
+
+    median_index = df_employees.shape[0] // 2
+    middle_salaries = df_employees.iloc[median_index - 2: median_index + 2]
+
+    df_employees_sample = df_employees.sample(frac=0.1)
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(df_employees_sample.index, df_employees_sample['Salary'], color='blue', label='Employees')
+
+    plt.scatter(bottom_5.index, bottom_5['Salary'], color='red', label='Bottom 5 Salaries')
+    plt.scatter(top_5.index, top_5['Salary'], color='green', label='Top 5 Salaries')
+    plt.scatter(middle_salaries.index, middle_salaries['Salary'], color='purple', label='Middle Salaries')
+
+    plt.title('Salaries of Employees')
+    plt.xlabel('Employee Index')
+    plt.ylabel('Salary')
+    plt.legend()
+    plt.show()
+
+if __name__ == '__main__':
+    number_of_employees()
+    info()
+    completed_rows()
+    min_max_mean_values()
+    unique_values()
+    valori_lipsa()
+    distributia_salariilor()
+    distributia_salariilor_echipe()
+    outliners()
---
+++ Inteligenta artificiala/Lab2/pb3.py
@@ -0,0 +1,85 @@
+import re
+
+import nltk
+import spacy as spacy
+from nltk.corpus import wordnet
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from rowordnet import RoWordNet
+from unidecode import unidecode
+
+
+def visualize_sentence_count(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+        sentences = sent_tokenize(text)
+        print("Number of sentences: ", len(sentences))
+
+
+def visualize_word_count(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+
+    words = word_tokenize(text)
+    print("Number of words: ", len(words))
+
+
+def visualize_unique_word_count(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+
+    words = word_tokenize(text)
+    unique_words = set(words)
+    print("Number of unique words: ", len(unique_words))
+
+
+def visualize_word_lengths(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+
+    cleaned_text = re.sub(r'[.,:;!?]', '', text)
+    words = word_tokenize(cleaned_text)
+    shortest_word = min(words, key=len)
+    longest_word = max(words, key=len)
+    print("Shortest word: ", shortest_word)
+    print("Longest word: ", longest_word)
+
+
+def remove_diacritics(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+
+    text_without_diacritics = unidecode(text)
+    print("Text without diacritics: ", text_without_diacritics)
+
+
+def synonyms_longest_word(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+
+    cleaned_text = re.sub(r'[.,:;!?]', '', text)
+    words = word_tokenize(cleaned_text)
+    longest_word = str(max(words, key=len))
+    #"ro_core_news_sm"
+    nlp = spacy.load("C:/Users/user/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.9_qbz5n2kfra8p0/LocalCache/local-packages/Python39/site-packages/spacy/data/ro_core_news_sm")
+    doc = nlp(longest_word)
+    singular_form = ""
+    for token in doc:
+        singular_form = token.lemma_ + " "
+    word = singular_form.strip()
+    rown = RoWordNet()
+    synset_ids = rown.synsets(literal=word)
+    for synset_id in synset_ids:
+        synset = rown.synset(synset_id)
+        synonyms = [literal for literal in synset.literals]
+        print(synonyms)
+
+
+#visualize_sentence_count('texts.txt')
+#visualize_word_count('texts.txt')
+#visualize_unique_word_count('texts.txt')
+#remove_diacritics('texts.txt')
+synonyms_longest_word('texts.txt')
+
+
+
